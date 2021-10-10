9ac1b8e153a661254129300d7b83d85a52e5d3b7
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-10-10 15:46:35 -0500

    💄 (dashboard): menu item icons

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,16 +1,13 @@
 import 'package:agro_app/app/core/utils/helpers.dart';
 import 'package:agro_app/app/core/values/app_colors.dart';
+import 'package:agro_app/app/modules/dashboard/local_widgets/menu_item.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:styled_widget/styled_widget.dart';
-
 import '../controllers/dashboard_controller.dart';
 
 class DashboardView extends GetView<DashboardController> {
   @override
   Widget build(BuildContext context) {
-    final _size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('DashboardView'),
@@ -88,72 +85,6 @@ class DashboardView extends GetView<DashboardController> {
                   ),
                 ],
               ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MenuItemCard extends StatefulWidget {
-  final String iconImage;
-  final String title;
-  MenuItemCard({
-    Key? key,
-    required this.iconImage,
-    required this.title,
-  }) : super(key: key);
-  @override
-  State<MenuItemCard> createState() => _MenuItemCardState();
-}
-
-class _MenuItemCardState extends State<MenuItemCard> {
-  bool pressed = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final menuItem = ({Widget? child}) => Styled.widget(child: child)
-        .alignment(Alignment.center)
-        .borderRadius(all: 15)
-        .ripple()
-        .backgroundColor(Colors.white, animate: true)
-        .clipRRect(all: 25) // clip ripple
-        .borderRadius(all: 25, animate: true)
-        .elevation(
-          pressed ? 0 : 20,
-          borderRadius: BorderRadius.circular(25),
-          shadowColor: Color(0x30000000),
-        ) // shadow borderRadius
-        .constrained(height: 200)
-        .padding(all: 10)
-        .gestures(
-          onTapChange: (tapStatus) => setState(() => pressed = tapStatus),
-          onTapDown: (details) => print('tapDown'),
-          onTap: () => print('onTap'),
-        )
-        .scale(all: pressed ? 0.95 : 1.0, animate: true)
-        .animate(Duration(milliseconds: 150), Curves.easeOut);
-    return menuItem(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
-            height: 80.0,
-            width: 80.0,
-            image: AssetImage(
-              widget.iconImage,
-            ),
-          ),
-          SizedBox(
-            height: 5.0,
-          ),
-          Text(
-            widget.title,
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              color: AppColors.valhalla,
             ),
           ),
         ],
